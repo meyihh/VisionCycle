f25e2cabe86793c5b17cb137c15addc17c5861ad
lucasbenedict-arch <lucasbenedict.bsit@gmail.com>
2026-03-10 07:31:51 +0800

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<!-- Logout Modal -->
+<div class="logout-overlay" id="logoutOverlay">
+    <div class="logout-modal">
+        <div class="modal-title">Logout</div>
+        <div class="modal-message">Are you sure you want to logout?</div>
+        <button class="btn-no"  id="btnNo">No</button>
+        <button class="btn-yes" id="btnYes">Yes</button>
+    </div>
+</div>
